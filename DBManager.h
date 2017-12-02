03096e5790275fc547bd16120b7f5e0596329b62
Seneca Meeks <senecacmeeks@gmail.com>
2017-12-01 23:54:56 -0500

    Add files via upload
    DBManager

---
+++ DBManager.h
@@ -0,0 +1,32 @@
+//
+//  DBManager.h
+//  GymBuddy1.0
+//
+//  Created by Seneca Meeks on 2/4/17.
+//  Copyright © 2017 Hidden Figures. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+-(instancetype)initWithDatabaseFilename:(NSString *)dbFilename;
+
+@property (nonatomic, strong) NSMutableArray *arrColumnNames;
+
+@property (nonatomic) int affectedRows;
+
+@property (nonatomic) long long lastInsertedRowID;
+
+@property (nonatomic, strong) NSString *documentsDirectory;
+@property (nonatomic, strong) NSString *databaseFilename;
+@property (nonatomic, strong) NSMutableArray *arrResults;
+-(void)copyDatabaseIntoDocumentsDirectory;
+-(void)runQuery:(const char *)query isQueryExecutable:(BOOL)queryExecutable;
+-(NSArray *)loadDataFromDB:(NSString *)query;
+-(void)executeQuery:(NSString *)query;
+
+
+
+@end
+
