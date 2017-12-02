ca6b0f5a03f3713edafbcacb46745b6cd9a206b1
Seneca Meeks <senecacmeeks@gmail.com>
2017-12-01 23:51:48 -0500

    Add files via upload
    More database files and a ViewController

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GymBuddy1.0
+//
+//  Created by Seneca Meeks on 2/4/17.
+//  Copyright © 2017 Hidden Figures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: Properties
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
